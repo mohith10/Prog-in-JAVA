3122e6b17ddc7f43e4420c29b4708f14bbe28f8a
mohith10 <mhooda87@gmail.com>
2017-07-15 15:51:25 +0530

    Basic Program

---
+++ Intro1.java
@@ -0,0 +1,16 @@
+/*
+            Program to find sum of two numbers.
+ */
+class Intro1
+{
+	public static void main(String args[])
+	{
+		int a=10,b=15,sum;
+		sum = a+b;
+		System.out.println("The sum of a,b is:"+sum);
+		if(sum%2 == 0)
+			System.out.println("Sum is Even!");
+		else
+			System.out.println("Sum is Odd!");
+	}
+}
