8e46e728d948b3f27378ace62d13c05a880fd546
mohith10 <mhooda87@gmail.com>
2017-07-16 17:18:13 +0530

    Few more BAsic

---
+++ Intro2.java
@@ -0,0 +1,15 @@
+/*
+            File Name: 16241A11
+            Roll Number: u4cse16241
+            Name: Mohit Hooda
+            Submitted on: 13 July
+ */
+class ass3
+{
+	public static void main(String args[])
+	{
+		int i;
+		for(i=0; i<10; i++)
+			System.out.println("i="+i);
+	}
+}
---
+++ Intro3.java
@@ -0,0 +1,16 @@
+/*
+            File Name: 16241A11
+            Roll Number: u4cse16241
+            Name: Mohit Hooda
+            Submitted on: 13 July
+ */
+class ass1
+{
+	public static void main(String args[])
+	{
+		int i,sum = 0;
+		for(i=1; i<=10; i++)
+			sum = sum+i;
+		System.out.println("Sum of first ten numbers ="+sum);
+	}	
+}
