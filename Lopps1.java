03b0d64c4fdd9b5ff4677ffdc6b72f0b38393910
mohith10 <mhooda87@gmail.com>
2017-07-18 16:12:43 +0530

    Loops

---
+++ Lopps1.java
@@ -0,0 +1,24 @@
+/*
+           Q.Write a JAVA Program to print multiplication table for given number upto 20.
+*/
+       import java.util.Scanner;
+
+public class lab1b
+{
+		public static void main(String args[])
+		{
+			Scanner sc=new Scanner(System.in);
+			int i,n;
+			System.out.print("Enter the number :");
+			n=sc.nextInt();
+			for(i=1;i<=20;i++)
+			{
+				System.out.println(i+"*"+n+"="+i*n);
+			}
+
+
+
+
+		}
+
+}
