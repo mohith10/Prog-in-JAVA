bc1c903e10080082dba635dbff4eb9b90d586621
mohith10 <mhooda87@gmail.com>
2017-07-19 16:34:10 +0530

    Loops 2

---
+++ Loops2.java
@@ -0,0 +1,40 @@
+
+            import java.util.Scanner;
+
+public class lab1d
+{
+		public static void main(String args[])
+		{
+			Scanner sc=new Scanner(System.in);
+			int i,n1,sum1=0,n2,sum2=0;
+			
+			System.out.print("Enter the 1st number :");
+			n1=sc.nextInt();
+			System.out.print("Enter the 2nd number :");
+			n2=sc.nextInt();
+			for(i=1;i<n1;i++)
+			{
+				if(n1%i==0)
+				{
+					sum1=sum1+i;
+				}
+			}
+			for(i=1;i<n2;i++)
+			{
+				if(n1%i==0)
+				{
+					sum2=sum2+i;
+				}
+			}
+
+			if(sum1==sum2)
+			{
+				System.out.println("Amicable Number");
+			}
+			else
+				System.out.println("Not an amicable number!");
+
+
+		}
+
+}
